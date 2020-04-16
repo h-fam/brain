53fe2eec4dd3f0bc9d61b768715f58b05252f2b9
Marcus Hines <marcus.hines@gmail.com>
2020-04-16 14:35:11 -0700

    update fully working version

--- interview/connected_cells/main.go
+++ interview/connected_cells/main.go
@@ -10,21 +10,64 @@ import (
 	"strings"
 )
 
+type Children []pos
+
+func (c Children) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c Children) Less(i, j int) bool {
+	return c[i].Less(c[j])
+}
+
+func (c Children) Len() int {
+	return len(c)
+}
+
+func (c *Children) Get() Children {
+	if c == nil {
+		return nil
+	}
+	return *c
+}
+func (c *Children) Add(p pos) {
+	e := *c
+	if len(e) == 0 {
+		*c = Children{p}
+	}
+	for i, v := range e {
+		if v == p {
+			return
+		}
+		if p.Less(v) {
+			*c = append(e[:i], append(Children{p}, e[i:]...)...)
+			return
+		}
+	}
+	*c = append(e, p)
+}
+
+func (c *Children) Pop() pos {
+	v := (*c)[0]
+	*c = (*c)[1:]
+	return v
+}
+
 type Graph struct {
 	nodes map[pos]bool
-	edges map[pos][]pos
+	edges map[pos]*Children
 }
 
 func (g Graph) String() string {
 	s := "Edges:\n"
-	var sPos []pos
+	var sPos Children
 	for v := range g.nodes {
 		sPos = append(sPos, v)
 	}
-	sort.Slice(sPos, func(i, j int) bool { return sPos[i].Less(sPos[j]) })
+	sort.Sort(sPos)
 	for _, k := range sPos {
 		var a []string
-		for _, v := range g.edges[k] {
+		for _, v := range g.edges[k].Get() {
 			a = append(a, fmt.Sprintf("%+v", v))
 		}
 		s += fmt.Sprintf("%+v: [%s]\n", k, strings.Join(a, ","))
@@ -33,24 +76,51 @@ func (g Graph) String() string {
 }
 
 func (g *Graph) Add(src, dst pos) {
-	e := g.edges[src]
-	if len(e) == 0 {
-		g.edges[src] = []pos{dst}
+	if _, ok := g.edges[src]; !ok {
+		g.edges[src] = &Children{}
 	}
-	for i, v := range e {
-		if v == dst {
-			return
+	g.edges[src].Add(dst)
+}
+
+type node struct {
+	visited  bool
+	children map[pos]bool
+}
+
+func (g *Graph) find(p pos) Children {
+	unvisited := &Children{p}
+	var children Children
+	visited := map[pos]bool{}
+	for unvisited.Len() > 0 {
+		c := unvisited.Pop()
+		children = append(children, c)
+		if visited[c] {
+			continue
 		}
-		if dst.Less(v) {
-			g.edges[src] = append(e[:i], append([]pos{dst}, e[i:]...)...)
-			return
+		visited[c] = true
+		for _, v := range g.edges[c].Get() {
+			if visited[v] {
+				continue
+			}
+			unvisited.Add(v)
 		}
 	}
-	g.edges[src] = append(e, dst)
+	return children
 }
 
 func (g *Graph) Max() int32 {
-	return 0
+	var unvisited []pos
+	for k := range g.nodes {
+		unvisited = append(unvisited, k)
+	}
+	max := 1
+	for _, v := range unvisited {
+		connected := g.find(v)
+		if len(connected) > max {
+			max = len(connected)
+		}
+	}
+	return int32(max)
 }
 
 type pos struct {
@@ -68,9 +138,9 @@ var adjPairs = []pos{
 	{-1, -1},
 	{0, -1},
 	{1, -1},
-	{0, -1},
-	{0, 1},
-	{1, 1},
+	{-1, 0},
+	{1, 0},
+	{-1, 1},
 	{0, 1},
 	{1, 1},
 }
@@ -87,7 +157,7 @@ func connectedCell(matrix [][]int32) int32 {
 		}
 	}
 	g := Graph{
-		edges: map[pos][]pos{},
+		edges: map[pos]*Children{},
 		nodes: valid,
 	}
 	for p := range valid {
