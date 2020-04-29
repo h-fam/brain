bda6ce3fe14cc72b13ea0a596f9c129842604ff6
Marcus Hines <marcus.hines@gmail.com>
2020-04-29 10:59:58 -0700

    make it work

--- interview/graph/even_tree/main.go
+++ interview/graph/even_tree/main.go
@@ -5,13 +5,99 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-// Complete the evenForest function below.
-func evenForest(t_nodes int32, t_edges int32, t_from []int32, t_to []int32) int32 {
+type Graph map[int32]Edges
 
+func (g Graph) AddEdge(src, dst int32) {
+	n, ok := g[src]
+	if !ok {
+		n = Edges{}
+		g[src] = n
+	}
+	n[dst] = true
+}
+
+func (g Graph) AddNode(n int32) {
+	g[n] = Edges{}
+}
+
+func (g Graph) String() string {
+	s := "Edges:\n"
+	var keys []int32
+	for v := range g {
+		keys = append(keys, v)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	for _, k := range keys {
+		var e []string
+		for k := range g[k] {
+			e = append(e, fmt.Sprintf("%d", k))
+		}
+		s += fmt.Sprintf("%d: [%s]\n", k, strings.Join(e, ","))
+	}
+	return s
+}
+
+type node struct {
+	id       int32
+	parent   int32
+	children int32
+}
+
+func (g Graph) Walk() int32 {
+	visited := map[int32]node{}
+	g.walk(1, 0, visited)
+	cuts := int32(0)
+	for _, v := range visited {
+		if v.parent == 0 {
+			continue
+		}
+		if v.children%2 == 0 {
+			cuts++
+		}
+	}
+	return cuts
+}
+
+func (g Graph) walk(n, p int32, visited map[int32]node) int32 {
+	curr, ok := visited[n]
+	if !ok {
+		curr = node{
+			id: n,
+		}
+		visited[n] = curr
+	}
+	c := int32(1)
+	for k := range g[n] {
+		if _, ok := visited[k]; ok {
+			continue
+		}
+		c += g.walk(k, n, visited)
+	}
+	curr.children = c
+	curr.parent = p
+	visited[n] = curr
+	return c
+}
+
+type Edges map[int32]bool
+
+// Complete the evenForest function below.
+func evenForest(tNodes int32, tEdges int32, tFrom []int32, tTo []int32) int32 {
+	g := Graph{}
+	for i := int32(1); i <= tNodes; i++ {
+		g.AddNode(i)
+	}
+	for i := range tFrom {
+		g.AddEdge(tFrom[i], tTo[i])
+		g.AddEdge(tTo[i], tFrom[i])
+	}
+	fmt.Println(g)
+	return g.Walk()
 }
 
 func main() {
@@ -47,7 +133,7 @@ func main() {
 		tTo = append(tTo, int32(edgeTo))
 	}
 
-	res := evenForest(t_nodes, t_edges, t_from, t_to)
+	res := evenForest(int32(tNodes), int32(tEdges), tFrom, tTo)
 
 	fmt.Fprintf(writer, "%d\n", res)
 
